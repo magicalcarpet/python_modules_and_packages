663a957674c41d0dc33e3f6ca7eefe4c808606b4
magicalcarpet <oke.ade@gmail.com>
2020-10-10 21:43:18 +0100

    Update experiment.py

--- app/inner_app/inner_inner_app/experiment.py
+++ app/inner_app/inner_inner_app/experiment.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# Full path is the path of the current file , that is the root to the current file.
+# Full path is the path of the current file, that is the root to the current file.
 full_path = os.path.realpath(__file__)
 
 
